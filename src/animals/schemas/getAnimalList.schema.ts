5a8d6be9d13886486325aeb9b019a8e425fce8a9
Matteo Soresini <matteo.soresini@fleetmatica.com>
2020-05-21 23:00:08 +0200

    removed useless code

--- src/animals/schemas/getAnimalList.schema.ts
+++ src/animals/schemas/getAnimalList.schema.ts
@@ -1,14 +1,6 @@
 import { Segments, Joi } from 'celebrate';
 import { updateAnimalKeys } from './updateAnimal.schema';
 
-const getAnimalListKeys = Joi.array().items(
-  Joi.object()
-    .keys({
-      ...updateAnimalKeys,
-    })
-    .required()
-);
-
 const body = Joi.array().items(Joi.object().keys(updateAnimalKeys).required());
 
 export default { [Segments.BODY]: body };
